ba586a5ea2bfb30ea49bedb15c288e5474a5e3c3
PandesalPanpan <petermarticio@gmail.com>
2025-04-26 17:01:24 +0800

    submit solution 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // Convert the string into a list of characters
+    const chars = string.split('');
+    let result = "";
+    // Loop starting from the end of the string
+    for (let i = string.length - 1; i >= 0; i--)
+    {
+        result += chars[i];
+    }
+    // Add the char one by one
+    // Return the result
+    return result;
 };
 
 // Do not edit below this line
