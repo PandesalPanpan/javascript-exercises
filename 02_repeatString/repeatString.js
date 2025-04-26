78c67912a753c6887bca4c7867789daf338b43c1
PandesalPanpan <petermarticio@gmail.com>
2025-04-26 16:51:41 +0800

    submit solution 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, repeat) {
+    // Negative numbers should return an error
+    if (repeat < 0) return "ERROR";
 
+    let result = "";
+    for (let i = 0; i < repeat; i++)
+    {
+        result += str;
+    }
+    return result;
 };
 
 // Do not edit below this line
