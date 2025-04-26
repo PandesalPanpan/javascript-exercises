bc888aa8120de06875ea3aaf09de5afb992805e8
PandesalPanpan <petermarticio@gmail.com>
2025-04-27 07:54:30 +0800

    submit solution 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elementsToRemove) {
+    // Check if elementToRemove exists in the array
+    let result = new Array();
+    return array.filter((element) => 
+    {
+        for (const elementToRemove of elementsToRemove)
+        {
+            if (element === elementToRemove)
+            {
+                return false;
+            }
+        }
+        return true;
+    });
+    
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
