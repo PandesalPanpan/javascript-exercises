d0f3b97d506a7ac0a98c7dc0e2ee9b24cd908a57
PandesalPanpan <petermarticio@gmail.com>
2025-04-28 11:01:40 +0800

    submit solution 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    // Check if the arguments are not negative
+    if (start < 0 
+        || end < 0 
+        || start % 1 !== 0 
+        || end % 1 !== 0
+        || typeof start !== typeof 0
+        || typeof end !== typeof 0
+    ) return "ERROR";
 
+    // Check which is bigger
+    if (start > end)
+    {
+        let buffer = start;
+        start = end;
+        end = buffer;
+    }
+
+    // Initialize a counter at 0
+    let sum = 0;
+    // Loop intial count = start, and once equal to end, ends
+    for (let i = start; i <= end; i++)
+    {
+        sum += i;
+    }
+    return sum; 
 };
 
 // Do not edit below this line
