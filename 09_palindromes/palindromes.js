979dc023b2a741258b2d6877eb0345e9ea5310bb
PandesalPanpan <petermarticio@gmail.com>
2025-05-04 09:41:48 +0800

    submit solution 09 palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // Extract to an array to only its characters without puncutations and spaces
+    let extractedString = string.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
 
+
+    // Copy the string -> Convert to Array -> reverse it -> Join it
+    let reversedString = extractedString.split('').reverse().join('');
+
+    // Compare Original and ReversedCopy
+    return extractedString === reversedString;
 };
 
 // Do not edit below this line
