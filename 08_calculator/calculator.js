4ac4926cd162f86bf0feb1b1216383b157b425bc
PandesalPanpan <petermarticio@gmail.com>
2025-05-04 09:22:09 +0800

    submit solution 08 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(...nums) {
+	return nums.reduce((sum, number) => sum + number, 0);
 };
 
-const subtract = function() {
-	
+const subtract = function(...nums) {
+	return nums.reduce((difference, number) => difference - number);
 };
 
-const sum = function() {
-	
+const sum = function (array) { // The problem was using rest arguments 
+  return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, current) => product *= current);
 };
 
-const power = function() {
-	
+const power = function(number, exponent) {
+	return Math.pow(number, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let product = 1;
+  for (let i = 1; i <= number; i++)
+  {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
