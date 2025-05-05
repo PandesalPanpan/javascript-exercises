ec43b74581432e154bfbba0ab235d606fa6ed546
PandesalPanpan <petermarticio@gmail.com>
2025-05-05 09:06:59 +0800

    submit solution 12 findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+   // Get the age of each person to compare
+   // Sort by Age
+   const sortedByAge = people.sort((a, b) => {
+    // Check if yearOfDeath exists and use todays year instead
+    if (!a.yearOfDeath) a.yearOfDeath = (new Date).getFullYear();
+    if (!b.yearOfDeath) b.yearOfDeath = (new Date).getFullYear();
 
+    firstAge = a.yearOfDeath - a.yearOfBirth;
+    secondAge = b.yearOfDeath - b.yearOfBirth;
+    return firstAge - secondAge;
+   });
+   return sortedByAge.pop();
 };
 
 // Do not edit below this line
