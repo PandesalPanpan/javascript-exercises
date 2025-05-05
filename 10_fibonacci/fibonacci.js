3daf5eee2cd264cf8da93da30c0726a17d0db15d
PandesalPanpan <petermarticio@gmail.com>
2025-05-05 08:43:55 +0800

    submit solution 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (number < 0) return "OOPS";
 
+    let count = 2;
+    const fibonacci = [0, 1];
+    if (number < 2) // Return immediately since the position is initial
+    {
+        return fibonacci[number];
+    }
+
+
+    while(count <= number)
+    {
+        const current = fibonacci[count - 1]
+        const previous = fibonacci[count - 2];
+
+        fibonacci.push(current + previous);
+        count++;
+    }
+
+    // Returns the last one
+    return fibonacci.pop();
 };
 
 // Do not edit below this line
