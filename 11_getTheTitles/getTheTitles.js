d7f1a42a696a37356b2e2158683ee152b2267405
PandesalPanpan <petermarticio@gmail.com>
2025-05-05 08:48:57 +0800

    submit solution 11 getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,23 @@
-const getTheTitles = function() {
+const getTheTitles = function(arrayOfObjectBooks) {
+    // Get an object with properties
+    /*
+    const books = [
+        {
+            title: 'Book',
+            author: 'Name'
+        },
+        {
+            title: 'Book2',
+            author: 'Name2'
+        }
+        ]
+
+    Returns ['Book','Book2']
+    */
+
+    // Map and simply get the title
+    return arrayOfObjectBooks.map((bookObject) => bookObject.title);
+
 
 };
 
